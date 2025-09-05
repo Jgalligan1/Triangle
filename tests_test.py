6539aa64ad5088fec2bc2a7543ff6518bbeaf6e7
Jack Galligan <jgallig1@stevens.edu>
2025-09-05 01:05:35 -0400

    Uploading Triangle and Triangle_Tests

---
+++ tests_test.py
@@ -0,0 +1,25 @@
+import unittest
+import triangle
+
+class TriangleTest(unittest.TestCase):
+    def test_Equilateral(self):
+        self.assertEqual(triangle.Triangle.classify_Triangle(1,1,1),"not right Equilateral")
+        self.assertEqual(triangle.Triangle.classify_Triangle(4,4,4),"not right Equilateral")
+        self.assertEqual(triangle.Triangle.classify_Triangle(.5,.5,.5),"not right Equilateral")
+    def test_Isosceles(self):
+        self.assertEqual(triangle.Triangle.classify_Triangle(1,4,4),"not right Isosceles")
+        self.assertEqual(triangle.Triangle.classify_Triangle(7,7,1),"not right Isosceles")
+        self.assertEqual(triangle.Triangle.classify_Triangle(100,50,100),"not right Isosceles")
+    def test_Scalene(self):
+        self.assertEqual(triangle.Triangle.classify_Triangle(1,2,3),"not right Scalene")
+        self.assertEqual(triangle.Triangle.classify_Triangle(5,8,4),"not right Scalene")
+        self.assertEqual(triangle.Triangle.classify_Triangle(20,13,43),"not right Scalene")
+    def test_Right(self):
+        self.assertEqual(triangle.Triangle.classify_Triangle(5,12,13),"right Scalene")
+        self.assertEqual(triangle.Triangle.classify_Triangle(3,5,4),"right Scalene")
+        self.assertEqual(triangle.Triangle.classify_Triangle(20,13,43),"not right Scalene")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- triangle.py
+++ triangle.py
@@ -1,1 +1,17 @@
-print("Triangle")
+class Triangle:
+
+    def classify_Triangle(a,b,c):
+        out = ""
+        if(a*a + b*b == c*c or a*a+c*c == b*b or b*b+c*c == a*a):
+            out +="right"
+        else:
+            out+="not right"
+        if(a == b and a == c):
+            out +=" Equilateral"
+            return out
+        elif(a == b or a == c or b == c):
+            out +=" Isosceles"
+            return out
+        else:
+            out+=" Scalene"
+            return out
